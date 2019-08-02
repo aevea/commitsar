947efa5219192a4cdcc8ad759c38bd95cdc128de
Simon Prochazka <simon@fallion.net>
2019-08-03 00:28:21 +0200

    feat: CheckMessage
    - parses commit message to check conformity with conventional commits

---
+++ internal/text/check_message_title_test.go
@@ -0,0 +1,27 @@
+package text
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckMessageTitle(t *testing.T) {
+	tests := map[string]error{
+		"chore: add something":          nil,
+		"chore(ci): added new CI stuff": nil,
+		"feat: added a new feature":     nil,
+		"fix!: breaking change":         nil,
+		"fix(security)!: breaking":      nil,
+		"chore:really close":            errTitleNonConform,
+		"perf(): nope":                  errTitleNonConform,
+		"chore(: bad":                   errTitleNonConform,
+		": nope":                        errTitleNonConform,
+		"fix tests":                     errTitleNonConform,
+	}
+
+	for test, expected := range tests {
+		err := CheckMessageTitle(test)
+		assert.Equal(t, expected, err)
+	}
+}
