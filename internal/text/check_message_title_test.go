884cf2fa39caa4166dc99798306a3f4a69e44aa1
Simon Prochazka <simon@fallion.net>
2019-08-03 13:08:06 +0200

    fix(CheckMessage): fix edge cases in regex
    - fixes cases where `fix(stuff)(stuff): message` were possible
    - fixes cases where double `!` was possible

--- internal/text/check_message_title_test.go
+++ internal/text/check_message_title_test.go
@@ -8,16 +8,18 @@ import (
 
 func TestCheckMessageTitle(t *testing.T) {
 	tests := map[string]error{
-		"chore: add something":          nil,
-		"chore(ci): added new CI stuff": nil,
-		"feat: added a new feature":     nil,
-		"fix!: breaking change":         nil,
-		"fix(security)!: breaking":      nil,
-		"chore:really close":            errTitleNonConform,
-		"perf(): nope":                  errTitleNonConform,
-		"chore(: bad":                   errTitleNonConform,
-		": nope":                        errTitleNonConform,
-		"fix tests":                     errTitleNonConform,
+		"chore: add something":               nil,
+		"chore(ci): added new CI stuff":      nil,
+		"feat: added a new feature":          nil,
+		"fix!: breaking change":              nil,
+		"fix(security)!: breaking":           nil,
+		"fix!!: breaking":                    errTitleNonConform,
+		"fix(security)(stuff): should break": errTitleNonConform,
+		"chore:really close":                 errTitleNonConform,
+		"perf(): nope":                       errTitleNonConform,
+		"chore(: bad":                        errTitleNonConform,
+		": nope":                             errTitleNonConform,
+		"fix tests":                          errTitleNonConform,
 	}
 
 	for test, expected := range tests {
