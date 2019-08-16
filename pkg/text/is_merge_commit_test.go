f5872fe21f6778cc564d6574fe3ef84b0e102533
Simon Prochazka <simon@fallion.net>
2019-08-16 16:34:06 +0200

    tests: add additional merge commit format to test

--- pkg/text/is_merge_commit_test.go
+++ pkg/text/is_merge_commit_test.go
@@ -10,6 +10,7 @@ func TestIsMergeCommit(t *testing.T) {
 	tests := map[string]bool{
 		"Merge commit '900a395d573f2b046d0b901be22808bf55319fc7'\n": true,
 		"Merge branch 'master' into three\n":                        true,
+		"Merge branch 'master' into feature/something-word":         true,
 		"chore: something\n":                                        false,
 		"fix: test":                                                 false,
 	}
