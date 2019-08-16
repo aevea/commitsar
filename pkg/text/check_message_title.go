9f1b0d0f304ab90987e8741e34fb0c1350be22c2
Simon Prochazka <simon@fallion.net>
2019-08-16 11:42:05 +0200

    feat: MessageTitle
    - function to strip only the title from commit message

--- pkg/text/check_message_title.go
+++ pkg/text/check_message_title.go
@@ -6,14 +6,14 @@ import (
 )
 
 var (
-	titleRegex         = regexp.MustCompile(`^(?P<type>\w+?)(?P<scope>\(\w+\)?)?!?:\s(?P<message>.+)`)
-	errTitleNonConform = errors.New("message title does not conform to conventional commits")
+	expectedFormatRegex = regexp.MustCompile(`^(?P<type>\w+?)(?P<scope>\(\w+\)?)?!?:\s(?P<message>.+)$`)
+	errTitleNonConform  = errors.New("message title does not conform to conventional commits")
 )
 
 // CheckMessageTitle verifies that the message title conforms to
 // conventional commmit standard https://www.conventionalcommits.org/en/v1.0.0-beta.4/#summary
 func CheckMessageTitle(message string) error {
-	match := titleRegex.FindStringSubmatch(message)
+	match := expectedFormatRegex.FindStringSubmatch(message)
 	if match == nil {
 		return errTitleNonConform
 	}
