8e5c951ab83e04f8a2dcbb346d15414686b31be3
Simon Prochazka <simon@fallion.net>
2019-08-21 14:06:11 +0200

    fix: Kodiak style regex
    - improves regex to cover /
    - adds Merge master into renovate/docker-alpine-3.x style commit message compatibility

--- pkg/text/is_merge_commit.go
+++ pkg/text/is_merge_commit.go
@@ -4,8 +4,9 @@ import (
 	"regexp"
 )
 
-var mergeCommitRegex = regexp.MustCompile(`^Merge commit '(?P<hash>\w+)'`)
-var mergeBranchRegex = regexp.MustCompile(`^Merge branch '(?P<incoming>\w+)' into (?P<current>\w+)`)
+var mergeCommitRegex = regexp.MustCompile(`^Merge commit '(?P<hash>\S+)'`)
+var mergeBranchRegex = regexp.MustCompile(`^Merge branch '(?P<incoming>\w+)' into (?P<current>\S+)`)
+var kodiakMergeBranchRegex = regexp.MustCompile(`^Merge (?P<incoming>\w+) into (?P<current>\S+)`)
 
 // IsMergeCommit tests message string against expected format of a merge commit and returns true/false based on it
 func IsMergeCommit(message string) bool {
@@ -13,7 +14,9 @@ func IsMergeCommit(message string) bool {
 
 	mergeBranchMatch := mergeBranchRegex.FindStringSubmatch(message)
 
-	if mergeCommitMatch != nil || mergeBranchMatch != nil {
+	kodiakMergeBranchMatch := kodiakMergeBranchRegex.FindStringSubmatch(message)
+
+	if mergeCommitMatch != nil || mergeBranchMatch != nil || kodiakMergeBranchMatch != nil {
 		return true
 	}
 
