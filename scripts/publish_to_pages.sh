c490ba4ad448b8adcd568b168301edba5fd49f34
Simon Prochazka <simon@fallion.net>
2019-08-23 15:20:04 +0200

    fix(pages): disable git status check temporarily

--- scripts/publish_to_pages.sh
+++ scripts/publish_to_pages.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    git status
-    exit 1;
-fi
+# if [ "`git status -s`" ]
+# then
+#     echo "The working directory is dirty. Please commit any pending changes."
+#     git status
+#     exit 1;
+# fi
 
 echo "Deleting old publication"
 mkdir tmp
