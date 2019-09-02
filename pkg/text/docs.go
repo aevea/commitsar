7b2c2eea80d6ed2cab1c267bc5047e3e304c95c1
Simon Prochazka <simon@fallion.net>
2019-09-02 15:26:16 +0200

    docs: fix typo in text package

--- pkg/text/docs.go
+++ pkg/text/docs.go
@@ -1,2 +1,2 @@
-// Package text contains functions to parse Git Commit texts and some basic helpers for identifying type of commiit by its message.
+// Package text contains functions to parse Git Commit texts and some basic helpers for identifying type of commit by its message.
 package text
