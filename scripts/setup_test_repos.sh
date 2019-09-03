8501de0db7a037b1c34c2e67e9d98e33768eecb9
Simon Prochazka <simon@fallion.net>
2019-09-03 15:44:08 +0200

    chore: add git_tags test repo
    - provides a clean repo with some tags applied

--- scripts/setup_test_repos.sh
+++ scripts/setup_test_repos.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 rm -rf testdata/commits_on_branch
+rm -rf testdata/git_tags
 
 if [ ! -d './testdata/commits_on_branch' ]; then
-  echo 'testdata/commits_on_branch_test/ directory does not exist at the root; creating...'
   cd testdata
+
+  echo 'testdata/commits_on_branch_test/ directory does not exist at the root; creating...'
   git clone commits_on_branch.bundle
+  echo 'done'
+
+  echo 'testdata/git_tags/ directory does not exist at the root; creating...'
+  git clone git_tags.bundle
   echo 'done'
   
 else
