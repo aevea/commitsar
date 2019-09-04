5ebcf6c3121c19c9598fa214311d4b4b20deb875
Simon Prochazka <simon@fallion.net>
2019-09-04 17:25:22 +0200

    chore: fmt

--- pkg/history/current_branch.go
+++ pkg/history/current_branch.go
@@ -4,7 +4,6 @@ import (
 	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
-
 // CurrentBranch returns the reference HEAD is at right now
 func (g *Git) CurrentBranch() (*plumbing.Reference, error) {
 	head, err := g.repo.Head()
