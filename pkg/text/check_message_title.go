91e2d6de3d575e68adc8b7f32cdce579b041ccaa
Simon Prochazka <simon@fallion.net>
2019-09-16 23:36:44 +0200

    feat: breaking change errors
    - check_message_title will now warn when breaking change is missing a commit body
    - check_message_title will now error when BC body does not start with BREAKING CHANGE:

--- pkg/text/check_message_title.go
+++ pkg/text/check_message_title.go
@@ -9,9 +9,14 @@ var (
 	errCategoryMissing     = errors.New("category missing")
 	errCategoryWrongFormat = errors.New("category wrong format")
 	errScopeNonConform     = errors.New("malformed scope")
+	errMissingBCBody       = errors.New("breaking change must contain commit body")
+	errBCMissingText       = errors.New("breaking change commit body must start with BREAKING CHANGE: ")
 
 	// Fields such as category and chore should contain only word characters
 	fieldRegex = regexp.MustCompile(`^\w+$`)
+
+	// Commits with breaking changes should contain text with BREAKING CHANGE: at start
+	bcRegex = regexp.MustCompile(`^BREAKING CHANGE: `)
 )
 
 // CheckMessageTitle verifies that the message title conforms to
@@ -29,6 +34,16 @@ func CheckMessageTitle(commit Commit) error {
 	scopeMatch := fieldRegex.FindStringSubmatch(commit.Scope)
 	if commit.Scope != "" && scopeMatch == nil {
 		return errScopeNonConform
+	}
+
+	if commit.Breaking {
+		if commit.Body == "" {
+			return errMissingBCBody
+		}
+		bcMatch := bcRegex.FindStringSubmatch(commit.Body)
+		if bcMatch == nil {
+			return errBCMissingText
+		}
 	}
 
 	return nil
