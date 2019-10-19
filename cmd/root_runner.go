b82b6b92b5d36ef8b51a99d9dece66bb08a563df
Simon Prochazka <simon@fallion.net>
2019-10-19 20:01:33 +0200

    feat!: use new Git Repository
    BREAKING CHANGE: All Git related functions have been moved to a separate repository as a lot of code was used by release-notary, but not by commitsar. This meant having to release commitsar to fix issues specific to only release notary.

--- cmd/root_runner.go
+++ cmd/root_runner.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/commitsar-app/commitsar/pkg/history"
 	"github.com/commitsar-app/commitsar/pkg/text"
+	history "github.com/commitsar-app/git/pkg"
 	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
 	"gopkg.in/src-d/go-git.v4/plumbing"
