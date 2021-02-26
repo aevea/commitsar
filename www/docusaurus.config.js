50f98b6cdc1731ba0c5717d119d3c9012398beed
Robin Cussol <RobinCsl@users.noreply.github.com>
2021-02-26 16:15:03 +0100

    fix(www): make GitHub link point to this repo
    It was pointing to Docusaurus' otherwise.

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -22,7 +22,7 @@ module.exports = {
         },
         // { to: "blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/aevea/commitsar",
           label: "GitHub",
           position: "right",
         },
